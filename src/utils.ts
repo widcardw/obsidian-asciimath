e394654c86c1ea5e0fb9d5c3d0d4e94733e4e666
kwinso <ruslan.sot70@gmail.com>
2023-09-30 10:23:06 +0300

    fix: isLatexCode inconsistent results.

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,11 @@ function normalizeEscape(escape: string) {
   return escape.replace(/([$^\\.()[\]{}*?|])/g, '\\$1')
 }
 
-const latexRegex = /\\([A-Za-z0-9]){2,}/gm
-const texEmbedRegex = /tex".*"/
 // This function checks if the given code contains LaTeX code, but it's not AsciiMath embed.
 function isLatexCode(code: string): boolean {
+  const latexRegex = /\\([A-Za-z0-9]){2,}/gm
+  const texEmbedRegex = /tex".*"/
+
   return latexRegex.test(code) && !texEmbedRegex.test(code)
 }
 
