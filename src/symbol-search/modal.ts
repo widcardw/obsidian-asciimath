9b17dc4c03400dfa58e6768cc3d6eb1db72b9204
widcardw <lui747353367@qq.com>
2023-10-02 13:25:05 +0800

    chore: add suggestion info and placeholder

--- src/symbol-search/modal.ts
+++ src/symbol-search/modal.ts
@@ -3,7 +3,12 @@ import symbols from './symbols.json'
 
 // First is the AsciiMath symbol, second is LaTeX alternative
 // The third element is rendered as a preview
-type AsciiMathSymbol = [string, string] | [string, string, string]
+interface AsciiMathSymbol {
+  am: string
+  tex: string
+  rendered?: string
+  placeholder?: string
+}
 
 export class SymbolSearchModal extends SuggestModal<AsciiMathSymbol> {
   private callback: (sym: string) => void
@@ -11,21 +16,23 @@ export class SymbolSearchModal extends SuggestModal<AsciiMathSymbol> {
   // Returns all available suggestions.
   getSuggestions(query: string): AsciiMathSymbol[] {
     query = query.toLowerCase()
-    return symbols.filter(sym => sym.some(v => v.toLocaleLowerCase().includes(query))) as AsciiMathSymbol[]
+    return symbols.filter(sym => ([sym.am, sym.tex]).some(v => v.toLocaleLowerCase().includes(query))) as AsciiMathSymbol[]
   }
 
   // Renders each suggestion item.
-  renderSuggestion([am, latex, toBeRendered]: AsciiMathSymbol, el: HTMLElement) {
+  renderSuggestion({ am, tex, rendered, placeholder }: AsciiMathSymbol, el: HTMLElement) {
     el.classList.add('__asciimath-symbol-search-result')
 
     const text = el.createDiv()
-    text.createEl('div', { text: am })
-    text.createEl('small', { text: `LaTeX alternative: ${latex}` })
+    const amLine = text.createDiv()// text.createEl('div', { text: am })
+    amLine.createSpan({ text: am })
+    placeholder && amLine.createSpan({ text: ` ${placeholder}`, cls: '__asciimath-symbol-search-placeholder' })
+    text.createEl('small', { text: `LaTeX alternative: ${tex}` })
 
     el.createDiv('__asciimath-symbol-search-preview math', (el) => {
       el.innerHTML = `
         <mjx-container class="MathJax" jax="CHTML">
-        ${renderMath(typeof toBeRendered !== 'undefined' ? toBeRendered : latex, false).innerHTML}
+        ${renderMath(typeof rendered !== 'undefined' ? rendered : tex, false).innerHTML}
         </mjx-container>
       `
       finishRenderMath()
@@ -38,6 +45,6 @@ export class SymbolSearchModal extends SuggestModal<AsciiMathSymbol> {
 
   // Perform action on the selected suggestion.
   onChooseSuggestion(sym: AsciiMathSymbol) {
-    this.callback(sym[0])
+    this.callback(sym.am)
   }
 }
